05da51a81970e0ada6763556ab6cec260dc6f457
Sam <sam@sambhattacharyya.com>
2023-09-08 13:46:37 -0500

    Don't specify resolution in vertex shader of pixel shuffle

--- src/layers/pixel-shuffle.ts
+++ src/layers/pixel-shuffle.ts
@@ -40,7 +40,7 @@ class PixelShuffle2X extends Layer {
               vsOutput.position = vec4f(xy, 0.0, 1.0);
               vsOutput.tex_coord = xy*0.5 + 0.5;
               vsOutput.tex_coord.y = - 1.0* vsOutput.tex_coord.y  + 1.0;
-              vsOutput.tex_coord = vsOutput.tex_coord*512.0;
+              vsOutput.tex_coord = vsOutput.tex_coord*2.0;
               return vsOutput;
             }
           
@@ -50,11 +50,11 @@ class PixelShuffle2X extends Layer {
               
                @fragment fn fragmentMain(input: VertexShaderOutput) -> @location(0) vec4f {
                   
-                    let x_int  = i32(floor(input.tex_coord.x/256.0));
-                    let y_int  = i32(floor(input.tex_coord.y/256.0));
+                    let x_int  = i32(floor(input.tex_coord.x));
+                    let y_int  = i32(floor(input.tex_coord.y));
                     let c_index = x_int + y_int*2;
                     
-                    let value = textureLoad(inputTexture, vec2<i32>(i32(fract(input.tex_coord.x/256.0)*256.0), i32(fract(input.tex_coord.y/256.0)*256.0)), 0)[c_index];
+                    let value = textureLoad(inputTexture, vec2<i32>(i32(fract(input.tex_coord.x)*256.0), i32(fract(input.tex_coord.y)*256.0)), 0)[c_index];
                    
                     
                     return vec4f(value, value, value, 1.0);
