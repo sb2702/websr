1ae7220ced70372bedc7a5e1328cc04b4b5369f6
Sam <sam@sambhattacharyya.com>
2023-09-08 18:05:19 -0500

    Clean up Anime4K network

--- src/networks/anime4k/cnn-2x-s.ts
+++ src/networks/anime4k/cnn-2x-s.ts
@@ -1,11 +1,8 @@
 import WebGPUContext from "../../context";
 import Layer from "../../layers/base_layer";
 import NeuralNetwork from "../base_network";
-import RGB2YUV from "../../layers/rgb_2_yuv";
-import GuassianLayer from "../../layers/gaussian";
 import Anime4KConv3x4 from "../../layers/anime4k/conv2d-3x4";
 import Anime4KConv8x4 from "../../layers/anime4k/conv2d-8x4";
-import DummyLayer from "../../layers/dummy";
 import PixelShuffle2X from "../../layers/pixel-shuffle";
 
 
@@ -33,15 +30,9 @@ export default class Anime4KCNN2XS extends NeuralNetwork{
 
         const conv2d_last_tf = new Anime4KConv8x4(context.device, [context.texture('conv2d_2_tf')], context.texture('conv2d_last_tf'), weights['conv2d_last_tf']);
 
-        const dummy_layer = new DummyLayer(context.device, [], context.texture('dummy'));
-
-        const rgb2yuv = new RGB2YUV(context.device, [context.input], context.texture('yuv'));
-
-        const gaussian = new GuassianLayer(this.context.device, [context.texture('yuv')], context.texture('gaussian'));
-
         const pixel_shuffle = new PixelShuffle2X(context.device, [context.texture('conv2d_last_tf'), context.input], this.context.output);
 
-        layers.push(conv2d_tf, conv2d_1_tf, conv2d_2_tf, conv2d_last_tf, dummy_layer, rgb2yuv, gaussian, pixel_shuffle);
+        layers.push(conv2d_tf, conv2d_1_tf, conv2d_2_tf, conv2d_last_tf, pixel_shuffle);
 
         return layers;
 
