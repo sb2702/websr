35d52d3b0665e46e8a65c6f1745af8bf4b441ffa
Sam <sam@sambhattacharyya.com>
2023-09-15 16:07:41 -0500

    Added library target

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -6,6 +6,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: [ "./src/main.ts"],
+    mode: 'production',
     output: {
         library: "WebSR",
         libraryTarget: "umd",
