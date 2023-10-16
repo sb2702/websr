0c527b586c96a8647fef72be159ff4520a91c551
Sam <sam@sambhattacharyya.com>
2023-10-16 11:32:10 -0500

    Fixed compute shader demo issue

--- webpack.demo.config.js
+++ webpack.demo.config.js
@@ -39,7 +39,7 @@ module.exports = {
     plugins: [
 
         new HtmlWebpackPlugin({
-            template: 'src/demo/test.html'
+            template: 'src/demo/index.html'
         }),
 
         new CleanWebpackPlugin({
