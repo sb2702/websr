255f5efa4cfeca5d92ccd36b634344672f68398b
Sam <sam@sambhattacharyya.com>
2023-12-22 22:20:28 -0600

    Fixed issue with mixmatched input

--- src/layers/anime4k/display_3c.ts
+++ src/layers/anime4k/display_3c.ts
@@ -74,42 +74,7 @@ class DisplayLayer3C extends RenderLayer {
                       }            
             `
             });
-
-        console.log(`
-                
-                   ${this.defaultVertexShader()}
-                   @group(0) @binding(0) var<storage, read_write> inputBuffer0: array<vec4f>;
-                   @group(0) @binding(1) var<storage, read_write> inputBuffer1: array<vec4f>;
-                   @group(0) @binding(2) var<storage, read_write> inputBuffer2: array<vec4f>;
-                   @group(0) @binding(3) var inputTexture: ${externalTexture?  'texture_external': 'texture_2d<f32>'};
-                   @group(0) @binding(4) var ourSampler: sampler;
-                  
-                   @fragment fn fragmentMain(input: VertexShaderOutput) -> @location(0) vec4f {
-                      
-                        let x = ${this.resolution.width}.0*(input.tex_coord.x);
-                        let y = ${this.resolution.height}.0*(input.tex_coord.y);
-                        
-                        let y2 = u32(floor(y));
-                        let x2 = u32(floor(x));
-                        
-                        let i = y2*${Math.floor(this.resolution.width)} +  x2;
-                       
-                        let x_floor  = u32(fract(x)*2.0);
-                        let y_floor  = u32(fract(y)*2.0);
-                        
-                        //I don t know, I think this is right? I found this by trial and error
-                        let c_index: u32 = x_floor + y_floor*2;  
         
-                        let value = inputBuffer0[i][c_index];
-                        let value1 = inputBuffer1[i][c_index];
-                        let value2 = inputBuffer2[i][c_index];
-                        
-                        let bicubic = ${textureLoad};
-                        
-                        return bicubic + vec4f(value, value1, value2, value2);
-                    
-                      }            
-            `);
         this.pipeline =  this.device.createRenderPipeline(this.defaultPipelineConfig());
         this.bindGroup = this.defaultBindGroup();
         this.renderPassDescriptor = this.defaultRenderPassDescriptor();
@@ -194,8 +159,6 @@ class DisplayLayer3C extends RenderLayer {
         const entries: any[]  = [];
 
         this.inputs.forEach(function (input, i) {
-
-            console.log(i);
             if(input instanceof GPUExternalTexture){
                 entries.push({ binding: i, resource: input})
             } else if (input instanceof GPUTexture){
@@ -206,15 +169,9 @@ class DisplayLayer3C extends RenderLayer {
 
         });
 
-        entries[3].binding =3;
-
-
-
-
 
         entries.push({ binding: this.inputs.length, resource: this.sampler })
 
-        entries[4].binding = 4;
 
         return this.device.createBindGroup({
             layout: this.bind_group_layout,
